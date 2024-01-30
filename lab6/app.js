8b1b61dcbea8f6b44340e27afbe344a3d57540ca
lequocan948 <132972274+lequocan948@users.noreply.github.com>
2024-01-30 15:40:06 +0700

    andeptrai

---
+++ lab6/app.js
@@ -0,0 +1,8 @@
+import * as wuocan from './lab6.1.js';
+
+console.log("lab 6.1 và 6.2");
+console.log(wuocan.uppercaseString("hello"));
+console.log(wuocan.lowercaseString("WORLD"));
+console.log(wuocan.subtract(5, 4));
+
+
---
+++ lab6/lab6.1.js
@@ -0,0 +1,11 @@
+export const uppercaseString = (string) => {
+    return string.toUpperCase();
+}
+
+export const lowercaseString = (string) => {
+    return string.toLowerCase()
+}
+
+export function subtract(x, y){
+    return x - y;
+}
