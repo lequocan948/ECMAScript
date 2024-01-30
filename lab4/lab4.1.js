fe5bf49ae0d798fc65a6fba701b05a82a6f9b113
lequocan948 <132972274+lequocan948@users.noreply.github.com>
2024-01-30 18:56:53 +0700

    aaaaa

--- lab4/lab4.1.js
+++
@@ -1,8 +0,0 @@
-let promise = new Promise(function(resolve, reject) {
-    resolve(1);
-
-    setTimeout(() => resolve(2), 1000);
-});
-
-promise.then(alert);
-//alert ra 1 và sau khi đợi 1s thì cũng không ra 2.
