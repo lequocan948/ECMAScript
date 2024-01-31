16dd2cbb545fdf8af98de4be46fcc85c15095dc2
lequocan948 <132972274+lequocan948@users.noreply.github.com>
2024-01-31 16:44:40 +0700

    anle

--- lab3/lab3.2.js
+++
@@ -1,3 +0,0 @@
-const sumArray = arr => arr.reduce((total, num) => total + num, 0);
-const arr = [1,2,3,4,5,6,7];
-console.log(sumArray(arr)); // Kết quả mong muốn: 28
--- lab6/app.js
+++ lab6/app.js
@@ -1,8 +1,25 @@
 import * as wuocan from './lab6.1.js';
 
-console.log("lab 6.1 và 6.2");
-console.log(wuocan.uppercaseString("hello"));
-console.log(wuocan.lowercaseString("WORLD"));
-console.log(wuocan.subtract(5, 4));
+import * as wuocan2 from './lab6.2.js';
+
+import * as wuocan3 from './lab6.3.js';
+
+import {subtract} from './sum_funcition.js';
+
+console.log("lab 6.1");
+console.log(wuocan.uppercaseString("wuoc")); //Kết quả là WUOC
+console.log(wuocan.lowercaseString("AN")); //Kết quả là an
+
+console.log("lab 6.2");
+console.log(wuocan2.uppercaseString("hello")); //Kết quả là HELLO
+console.log(wuocan2.lowercaseString("WORLD")); //Kết quả là world
+
+console.log("lab 6.3");
+console.log(wuocan3.subtract(5, 4)); //Kết quả là 1
+
+console.log("lab 6.4");
+console.log(subtract(7, 4)); //kết quả là 11
+
+
 
 
