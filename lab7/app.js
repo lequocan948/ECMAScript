1220c12a063834568ef19dbff9338e39a2b972a0
lequocan948 <132972274+lequocan948@users.noreply.github.com>
2024-02-01 15:56:30 +0700

    aaaaaa

---
+++ lab7/app.js
@@ -0,0 +1,7 @@
+let chuoi = "Tôi là Wuocan";
+let chuoi2 = "Tôi là Wuocan";
+let chuoi3 = "Tôi là Wuocan";
+let chuoi4 = "Tôi là Wuocan";
+
+let chuoiCuiCung = "biết gì chưa?"+chuoi2+chuoi3+"123456"+chuoi4+"Biết chưa?";
+chuoiCuiCung;
